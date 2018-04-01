75837489a872e40aac573998a62a1e0fe351ca2f
owensm0132 <owensm0132@gmail.com>
2018-04-01 14:35:30 -0400

    separate sensitive data into separate config and add to gitignore

--- Fiasm/Fiasm/Startup/Program.cs
+++ Fiasm/Fiasm/Startup/Program.cs
@@ -19,6 +19,14 @@ namespace Fiasm
 
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration((builderContext, config) =>
+                {
+                    IHostingEnvironment env = builderContext.HostingEnvironment;
+                    config
+                        .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                        .AddJsonFile("appsettings_secrets.json", optional: false, reloadOnChange: true)
+                        .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true, reloadOnChange: true);
+                })
                 .UseStartup<Startup>()
                 .Build();
     }
